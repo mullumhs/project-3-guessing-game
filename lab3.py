d92339893745f6d2a7e454cebcf3d207fca98283
David Steedman <dave@voxace.com>
2023-10-06 21:59:15 +1100

    Project setup

---
+++ lab3.py
@@ -0,0 +1,21 @@
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+                                    LAB 3
+---------------------------------------------------------------------------------
+- File Name: lab3.py
+- Teacher: David Steedman
+- Class: Software Engineering
+- Description: Learn how to use loops and selection to validate user input.
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+# Use a while loop to continue prompting the user until a valid integer is entered. 
+# If the user enters something that's not an integer, display an error message.
+
+
+
+# Modify the previous exercise to accept an integer between 1 and 100. 
+# Use both a while loop and selection structures to ensure it is an integer and falls within the specified range.
+
+
+
+#  Write a loop that iterates through numbers from 1 to 100. 
+# The loop skips even numbers using continue and stops when it reaches 50 using break.
---
+++ lab4.py
@@ -0,0 +1,22 @@
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+                                    LAB 4
+---------------------------------------------------------------------------------
+- File Name: lab4.py
+- Teacher: David Steedman
+- Class: Software Engineering
+- Description: Learn how to generate random numbers in Python.
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+# Import the random module.
+
+
+
+# Print a random integer between 1 and 10.
+
+
+
+# Generate a random float between 0 and 1
+
+
+
+# Generate a random float between 1 and 10 and round it to 2 decimal places.
